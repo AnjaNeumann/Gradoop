9864879c33279abe5cb504088e5f6ac485f93033
anja <anja-neumann1@gmx.de>
2017-07-09 22:42:46 +0200

    aufgeräumt

---
+++ gradoop-examples/src/main/java/org/gradoop/metabolism/Main.java
@@ -0,0 +1,17 @@
+package org.gradoop.metabolism;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Metabolism mtb = new Metabolism(args);
+		String vcnt = mtb.getVertexCount();
+		String ecnt = mtb.getEdgeCount();
+
+		mtb.getVertexEdges();
+
+		System.out.println("VertexCount: " + vcnt);
+		System.out.println("EdgeCount: " + ecnt);
+
+	}
+
+}
--- gradoop-examples/src/main/java/org/gradoop/metabolism/Metabolism.java
+++ gradoop-examples/src/main/java/org/gradoop/metabolism/Metabolism.java
@@ -1,66 +1,101 @@
 package org.gradoop.metabolism;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
+import org.gradoop.common.model.api.entities.EPGMGraphHead;
 import org.gradoop.common.model.impl.id.GradoopId;
 import org.gradoop.common.model.impl.pojo.Edge;
 import org.gradoop.common.model.impl.pojo.Vertex;
 import org.gradoop.examples.AbstractRunner;
 import org.gradoop.flink.model.impl.LogicalGraph;
+import org.gradoop.flink.model.impl.operators.aggregation.functions.count.EdgeCount;
+import org.gradoop.flink.model.impl.operators.aggregation.functions.count.VertexCount;
 
 import com.google.common.base.Preconditions;
 
 public class Metabolism extends AbstractRunner {
 
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		Preconditions.checkArgument(
-			      args.length == 1, "input dir required");
-			    String inputDir  = args[0];
-			    System.out.println(args[0]);
+	LogicalGraph graph;
 
+	public Metabolism(String[] args) {
+		Preconditions.checkArgument(args.length == 1, "input dir required");
+		String inputDir = args[0];
+		System.out.println(args[0]);
 
-			    LogicalGraph graph = readLogicalGraph(inputDir);
-			    System.out.println("read graph");
-			    List<Edge> edges =graph.getEdges().collect();
-			    List<Vertex> vertices = graph.getVertices().collect();
-			    System.out.println("Edges: "+edges.size());
-			    System.out.println("Vertices: "+vertices.size());
-			    
-			    Map<Vertex, Integer> EdgesInCounts = new HashMap<>();
-			    Map<Vertex, Integer> EdgesOutCounts = new HashMap<>();
-			    Set<GradoopId> idSet = new HashSet<>();
-			    int cntIn, cntOut;
-			    for (Vertex vertex : vertices){
-			    	GradoopId vertexID = vertex.getId();
-			    	//System.out.println(vertexID);
-			    	
-			    	cntIn = 0;
-			    	cntOut = 0;
-			    	for(Edge edge : edges){
-			    		//System.out.println(edge.getSourceId());
-			    		idSet.add(edge.getSourceId());
-			    		if (edge.getSourceId().equals(vertexID)){
-			    			//System.out.println("Source -> "+edge.getSourceId());
-			    			cntOut ++;
-			    		}
-			    		if (edge.getTargetId().equals(vertexID)){
-			    			//System.out.println("Target -> "+edge.getSourceId());
-			    			cntIn ++;
-			    		}
-			    	}
-			    	EdgesInCounts.put(vertex, cntIn);
-			    	EdgesOutCounts.put(vertex, cntOut);
-			    	System.out.println("Vertex: "+vertex.getLabel()+":\t incoming Edges: "+ cntIn +"\t outgoing Edges: "+ cntOut);
-			    }
-			    
-
+		this.graph = readLogicalGraph(inputDir);
 	}
-	
-	
+
+	public String getVertexCount() throws Exception {
+		VertexCount vertexCount = new VertexCount();
+		graph = graph.aggregate(vertexCount);
+		EPGMGraphHead graphHead = graph.getGraphHead().collect().get(0);
+		return graphHead.getPropertyValue(vertexCount.getAggregatePropertyKey()).toString();
+	}
+
+	public String getEdgeCount() throws Exception {
+		EdgeCount edgeCount = new EdgeCount();
+		graph = graph.aggregate(edgeCount);
+		EPGMGraphHead graphHead = graph.getGraphHead().collect().get(0);
+		return graphHead.getPropertyValue(edgeCount.getAggregatePropertyKey()).toString();
+	}
+
+	public void getVertexEdges() throws Exception {
+
+		List<Edge> edges = graph.getEdges().collect();
+		List<Vertex> vertices = graph.getVertices().collect();
+		// System.out.println("Edges: "+edges.size());
+		// System.out.println("Vertices: "+vertices.size());
+
+		Map<Vertex, Integer> EdgesInCounts = new HashMap<>();
+		Map<Vertex, Integer> EdgesOutCounts = new HashMap<>();
+		int cntIn, cntOut;
+		for (Vertex vertex : vertices) {
+			GradoopId vertexID = vertex.getId();
+
+			cntIn = 0;
+			cntOut = 0;
+			for (Edge edge : edges) {
+				// if (edge.getSourceId().equals(vertexID)) {
+				if (edge.getTargetId().equals(vertexID)) {
+					EdgesInCounts.put(vertex, cntIn);
+					EdgesOutCounts.put(vertex, cntOut);
+
+					System.out.println("Vertex: " + vertex.getLabel() + ":\t incoming Edges: " + cntIn
+							+ "\t outgoing Edges: " + cntOut);
+
+				}
+			}
+		}
+	}
+
+	// public static void main(String[] args) throws Exception {
+	//
+	//
+	//
+	// List<Edge> edges =graph.getEdges().collect(); List<Vertex> vertices =
+	// graph.getVertices().collect();
+	// System.out.println("Edges: "+edges.size());
+	// System.out.println("Vertices: "+vertices.size());
+	//
+	// Map<Vertex, Integer> EdgesInCounts = new HashMap<>(); Map<Vertex,
+	// Integer> EdgesOutCounts = new HashMap<>(); Set<GradoopId> idSet = new
+	// HashSet<>(); int cntIn, cntOut; for (Vertex vertex : vertices){
+	// GradoopId vertexID = vertex.getId(); //System.out.println(vertexID);
+	//
+	// cntIn = 0; cntOut = 0; for(Edge edge : edges){
+	// //System.out.println(edge.getSourceId());
+	// idSet.add(edge.getSourceId()); if
+	// (edge.getSourceId().equals(vertexID)){
+	// System.out.println("Source -> "+edge.getSourceId()); cntOut ++; }
+	// if (edge.getTargetId().equals(vertexID)){
+	// //System.out.println("Target -> "+edge.getSourceId()); cntIn ++; } }
+	// EdgesInCounts.put(vertex, cntIn); EdgesOutCounts.put(vertex, cntOut);
+	// System.out.println("Vertex: "+vertex.getLabel()
+	// +":\t incoming Edges: "+ cntIn +"\t outgoing Edges: "+ cntOut);
+	// }
+	//
+	// }
 
 }
